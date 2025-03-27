0de96c994e861b6c3321e02eea7fa416673aac3f
dot\nikita.flimakov <nikita.flimakov@opesnoft.one>
2025-03-27 18:02:29 +0400

    0.1.0.20

--- src/DartWing/DartWing.Web/Program.cs
+++ src/DartWing/DartWing.Web/Program.cs
@@ -44,7 +44,7 @@ builder.Services.AddSwaggerGen(options =>
         Type = SecuritySchemeType.OAuth2,
         Flows = new OpenApiOAuthFlows
         {
-            /*Implicit = new OpenApiOAuthFlow
+            Implicit = new OpenApiOAuthFlow
             {
                 AuthorizationUrl = new Uri($"https://{builder.Configuration["KeyCloak:Domain"]}/realms/{builder.Configuration["KeyCloak:RealmName"]}/protocol/openid-connect/auth"),
                 Scopes = new Dictionary<string, string>
@@ -54,7 +54,7 @@ builder.Services.AddSwaggerGen(options =>
                     {"email", "Email"},
                     {"offline_access", "Offline Access"},
                 }
-            }*/
+            }
         }
     });
     
